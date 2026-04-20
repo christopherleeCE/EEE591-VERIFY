13b18f460abbdf543bf61f74afe3949539443d21
apcvSHIELD <andycox@asu.edu>
2026-04-20 14:36:07 -0700

    Update clean.sh from VNC

--- hw8/clean.sh
+++ hw8/clean.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
-# If in VNC
-# 1. Download via zip: https://github.com/christopherleeCE/EEE591-VERIFY/tree/main
-# 2. unzip EEE591-VERIFY-main.zip "EEE591-VERIFY-main/hw8/*" -d "homework08"
-
 # Configuration
-SOURCE_DIR="hw8"
+SOURCE_DIR="./"	# hw8 directory.
 CLEAN_DIR="hw8_clean"
 
 # Create the clean directory if it doesn't exist
@@ -23,7 +19,7 @@ for file in "$SOURCE_DIR"/*; do
     #    -c skips characters that cannot be converted.
     # 2. tr: The filter logic.
     #    -d (delete) -c (complement).
-    #    We keep: printable chars [:print:], Newline (\n), and Carriage Return (\r).
+    #    We keep: printable chars [:print:], Newline (\n), and Carriage Return $
 
     iconv -f UTF-8 -t MS-ANSI -c "$file" | \
     tr -dc '[:print:]\n\r' > "$CLEAN_DIR/$filename"
